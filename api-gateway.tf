dd65d3d35e7216f6ea27e5071bbdabf4caa83ef5
Marcello Cordeiro <maarc.hen@gmail.com>
2024-05-13 19:29:20 -0300

    update pipe

--- api-gateway.tf
+++ api-gateway.tf
@@ -57,18 +57,18 @@ resource "aws_api_gateway_integration_response" "api_integration_response" {
   }
 }
 
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.fastfood_api.id
-  resource_id = aws_api_gateway_resource.api_resource.id
-  http_method = "OPTIONS"
-  status_code = "200"
+# resource "aws_api_gateway_integration_response" "options_integration_response" {
+#   rest_api_id = aws_api_gateway_rest_api.fastfood_api.id
+#   resource_id = aws_api_gateway_resource.api_resource.id
+#   http_method = "OPTIONS"
+#   status_code = "200"
 
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Origin"      = "'*'"
-    "method.response.header.Access-Control-Allow-Credentials" = "'false'"
-  }
-}
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+#     "method.response.header.Access-Control-Allow-Origin"      = "'*'"
+#     "method.response.header.Access-Control-Allow-Credentials" = "'false'"
+#   }
+# }
 
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on  = [aws_api_gateway_integration.gateway_integration]
