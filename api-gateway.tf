d14b690a2597378896ba0f30129c48a8c5e15cfb
Marcello Cordeiro <maarc.hen@gmail.com>
2024-05-13 19:17:43 -0300

    feat: added success template response for api gateway

--- api-gateway.tf
+++ api-gateway.tf
@@ -39,6 +39,37 @@ resource "aws_api_gateway_integration" "gateway_integration" {
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
+resource "aws_api_gateway_method_response" "api_response" {
+  rest_api_id = aws_api_gateway_rest_api.fastfood_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = aws_api_gateway_method.api_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "api_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.fastfood_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = aws_api_gateway_method.api_method.http_method
+  status_code = aws_api_gateway_method_response.api_response.status_code
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.fastfood_api.id
+  resource_id = aws_api_gateway_resource.api_resource.id
+  http_method = "OPTIONS"
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Origin"      = "'*'"
+    "method.response.header.Access-Control-Allow-Credentials" = "'false'"
+  }
+}
+
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on  = [aws_api_gateway_integration.gateway_integration]
   rest_api_id = aws_api_gateway_rest_api.fastfood_api.id
